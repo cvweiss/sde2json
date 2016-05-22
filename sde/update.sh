d595a0c68bd54c40dbabef0eca9814219cb1983a
www-data <www-data@ubuntu.members.linode.com>
2016-05-22 22:52:42 +0000

    uncommented the things

--- sde/update.sh
+++ sde/update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#rm -rf sde-*
-#rm mysql-latest.tar.bz2
-#rm mysql-latest.tar
+rm -rf sde-*
+rm mysql-latest.tar.bz2
+rm mysql-latest.tar
 
-#wget "https://www.fuzzwork.co.uk/dump/mysql-latest.tar.bz2"
-#bunzip2 "mysql-latest.tar.bz2"
-#tar xf mysql-latest.tar
+wget "https://www.fuzzwork.co.uk/dump/mysql-latest.tar.bz2"
+bunzip2 "mysql-latest.tar.bz2"
+tar xf mysql-latest.tar
 
 cd sde-*
 mysql -usde -psde sde < sde-*.sql
